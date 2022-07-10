2d177e20308255b51e023a7fb9eaff657cf7ceb1
alexpitzen <alexpitzen@gmail.com>
2022-07-09 22:44:14 -0500

    Shiny Wally tutorial opponent

--- src/pokemon.c
+++ src/pokemon.c
@@ -10019,7 +10019,7 @@ void CreateMaleMon(struct Pokemon *mon, u16 species, u8 level)
         personality = Random32();
     }
     while (GetGenderFromSpeciesAndPersonality(species, personality) != MON_MALE);
-    CreateMon(mon, species, level, USE_RANDOM_IVS, TRUE, personality, OT_ID_PRESET, otId);
+    CreateMon(mon, species, level, USE_RANDOM_IVS, TRUE, otId, OT_ID_PRESET, otId);
 }
 
 void CreateMonWithIVsPersonality(struct Pokemon *mon, u16 species, u8 level, u32 ivs, u32 personality)
